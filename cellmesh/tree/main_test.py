5c6c7414707b441622cd82b810a77387a8654d40
Shunfu Mao <shunfu@uw.edu>
2019-04-06 16:31:35 -0700

    add some examples from test_100x_400_new

--- cellmesh/tree/main_test.py
+++ cellmesh/tree/main_test.py
@@ -17,7 +17,20 @@ def test_iMeSH_D_Tree():
 
   # show subtree for two mids
   # modify "1", "2" etc to any tag strings you want
-  mid_list = [("D011901", "1"), ("D019599", "2")]
+  #mid_list = [("D011901", "1"), ("D019599", "2")]
+
+  #http://uncurl-app.yjzhang.com:8888/user/test_10x_400_new/view
+  # issues:
+  # - query genes: may be missing (synonym?) or not occurred in literature?
+  # - ref cell: naming strategy CD8+ cytotoxic t vs CD8+/CD45RA+ naive cytotoxic
+  # cluster 1
+  mid_list = [("D009000", "ref"), ("D002999", "1"), ("D016176", "2"), ("D007694", "3")]
+  # cluster 5
+  #mid_list = [("D013602", "ref"), ("D004912", "1"), ("D020298", "2"), ("D007694", "3"), ("D003713", "4"), ("D015603", "5")]
+  # cluster 6 CD4+/CD45RO+ memory t
+  #mid_list = [("D016176", "ref"), ("D004912", "1"), ("D015603", "2"), ("D019169", "3")]
+  # cluster 7 CD8+/CD45RA+ naive cytotoxic
+  #mid_list = [("D013602", "ref"), ("D020298", "1"), ("D007694", "2"), ("D003713", "3")]
   st = iMT.tree_str(mid_list)
   print("\n\nsubtree for mid_list=%s"%str(mid_list))
   print(st)
