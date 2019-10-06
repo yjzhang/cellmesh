adf30ac9aaa0f8f4a7f92891da7f7926250a3950
Yue Zhang <yjzhang@cs.washington.edu>
2019-10-05 23:58:54 -0700

    added anatomy terms

---
+++ parse_cellmesh_anatomy.py
@@ -0,0 +1,149 @@
+from collections import Counter
+import json
+import sqlite3
+import os
+
+import traceback
+
+path = 'cnt_AllAnatomy_T0_RowNormNone_ColNormNone'
+
+# 1. open db
+conn_db = sqlite3.connect(os.path.join(path, 'corpus.db'))
+cursor_db = conn_db.cursor()
+
+# 1.5. load gene and mesh info from corpus.db
+cursor_db.execute('SELECT * FROM gene_dict')
+gene_list = cursor_db.fetchall()
+gene_dict = {x[2]: x for x in gene_list}
+cursor_db.execute('SELECT * FROM cell_dict')
+cell_list = cursor_db.fetchall()
+cell_dict = {x[1]: x for x in cell_list}
+
+# TODO: construct hierarchies
+tree_ids_dict = {}
+# children_dict includes both direct and indirect children.
+children_dict = {}
+direct_children_dict = {}
+parents_dict = {}
+direct_parents_dict = {}
+root_items = []
+for x in cell_list:
+    cell_id = x[1]
+    tree_id = x[3]
+    tree_ids_dict[tree_id] = cell_id
+    if cell_id not in children_dict:
+        children_dict[cell_id] = set()
+        direct_children_dict[cell_id] = set()
+        parents_dict[cell_id] = set()
+        direct_parents_dict[cell_id] = set()
+for tree_id, cell_id in tree_ids_dict.items():
+    tree_sub_components = tree_id.split('.')
+    if len(tree_sub_components) == 1:
+        root_items.append(cell_id)
+    for i in range(1, len(tree_sub_components) + 1):
+        sub_tree_id = '.'.join(tree_sub_components[:i])
+        if sub_tree_id in tree_ids_dict:
+            cell_id_parent = tree_ids_dict[sub_tree_id]
+            children_dict[cell_id_parent].add(cell_id)
+            parents_dict[cell_id].add(cell_id_parent)
+            if i == len(tree_sub_components) - 1:
+                direct_children_dict[cell_id_parent].add(cell_id)
+                direct_parents_dict[cell_id].add(cell_id_parent)
+import numpy as np
+np.savetxt('cellmesh/data/root_mesh_ids.txt', root_items, fmt='%s')
+
+# 2. open json file
+with open(os.path.join(path, 'CoCount_gene2pubmed_(homo_sapiens_protein_coding)_MeSH_anatomy.json')) as f:
+    gene_mesh_pubmed = json.load(f)
+
+# dict of gene id : total count
+total_counts_dict = Counter()
+
+# 3. create new db
+output_db = sqlite3.connect('cellmesh/data/anatomy_mesh.db')
+output_cursor = output_db.cursor()
+
+# create a table representing a MeSH ID - gene mapping.
+try:
+    # pmids is a comma-separated string of ints
+    output_cursor.execute('CREATE TABLE cell_gene(cellID text, gene text, count integer, pmids text)')
+    # iterate over nonzero entries of corpus
+    for key, item in gene_mesh_pubmed.items():
+        s1 = key.split(',')
+        species = s1[0]
+        s2 = s1[1].split(':')
+        gene_id, mesh_id = s2
+
+        count = item['cnt']
+
+        cell_record = cell_dict[mesh_id]
+        gene_record = gene_dict[gene_id]
+        cell_id = cell_record[1]
+
+        gene = gene_record[3]
+        total_counts_dict[gene] += count
+        taxid = gene_record[1]
+        pmids = ','.join(item['place'])
+        output_cursor.execute('INSERT INTO cell_gene VALUES (?, ?, ?, ?)', (cell_id, gene, count, pmids))
+except Exception as e:
+    text = traceback.format_exc()
+    print(text)
+
+try:
+    output_cursor.execute('CREATE INDEX cell_gene_id_index ON cell_gene(cellID)')
+except:
+    pass
+
+
+# create a table representing a cellID - cellName mapping
+try:
+    output_cursor.execute('CREATE TABLE cell_name(cellID text, cellName text)')
+    for i, cell_record in cell_dict.items():
+        cell_id = i
+        cell_name = cell_record[2]
+        output_cursor.execute('INSERT INTO cell_name VALUES (?, ?)', (cell_id, cell_name))
+except Exception as e:
+    text = traceback.format_exc()
+    print(text)
+try:
+    output_cursor.execute('CREATE INDEX cell_name_id_index ON cell_name(cellID)')
+except:
+    pass
+
+# create a table representing a gene symbol - gene info mapping
+try:
+    output_cursor.execute('CREATE TABLE gene_info(gene text, geneID integer, totalCounts integer)')
+    for i, gene_record in gene_dict.items():
+        gene = gene_record[3]
+        gene_id = int(i)
+        total_counts = total_counts_dict[i]
+        output_cursor.execute('INSERT INTO gene_info VALUES (?, ?, ?)', (gene, gene_id, total_counts))
+except Exception as e:
+    text = traceback.format_exc()
+    print(text)
+try:
+    output_cursor.execute('CREATE INDEX gene_info_id_index ON gene_info(gene)')
+except:
+    pass
+
+# create a table representing a cell id - children mapping
+try:
+    output_cursor.execute('CREATE TABLE cell_children(cellID text, children text, directChildren text)')
+    for cell_id, children in children_dict.items():
+        children_text = ','.join(children)
+        direct_children_text = ','.join(direct_children_dict[cell_id])
+        output_cursor.execute('INSERT INTO cell_children VALUES (?, ?, ?)', (cell_id, children_text, direct_children_text))
+except Exception as e:
+    text = traceback.format_exc()
+    print(text)
+try:
+    output_cursor.execute('CREATE INDEX cell_children_id_index ON cell_children(cellID)')
+except:
+    pass
+
+
+output_cursor.execute('VACUUM')
+
+output_db.commit()
+output_db.close()
+
